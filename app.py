041144fe58e110af4f3f3507446fe7dfa19b0808
Ubuntu <ubuntu@ip-172-31-44-13.ap-south-1.compute.internal>
2026-05-18 06:50:39 +0000

    Initial Enterprise DevSecOps Pipeline

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
 @app.route('/')
 def home():
     return "Enterprise DevSecOps Pipeline Running Successfully"

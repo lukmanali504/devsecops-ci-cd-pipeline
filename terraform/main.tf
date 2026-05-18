d10a4d144175e29fd3d380aeb5a1b3792661296b
Ubuntu <ubuntu@ip-172-31-44-13.ap-south-1.compute.internal>
2026-05-18 10:14:20 +0000

    Checkov scan passed

--- terraform/main.tf
+++ terraform/main.tf
@@ -124,3 +124,30 @@ resource "aws_s3_bucket_replication_configuration" "replication" {
     }
   }
 }
+
+# =========================================================
+# LOG BUCKET VERSIONING
+# =========================================================
+
+resource "aws_s3_bucket_versioning" "log_bucket_versioning" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# =========================================================
+# LOG BUCKET KMS ENCRYPTION
+# =========================================================
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket_encryption" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_kms.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}

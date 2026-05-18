f34d79eb14590cc351a4cebe9c276d7f6906d631
Ubuntu <ubuntu@ip-172-31-44-13.ap-south-1.compute.internal>
2026-05-18 07:36:07 +0000

    No credentials detected

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
-AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
 @app.route('/')
 def home():

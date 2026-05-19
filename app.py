b350d330b893ffb60983e605df3b3267feca2076
Ubuntu <ubuntu@ip-172-31-44-13.ap-south-1.compute.internal>
2026-05-19 22:25:33 +0000

    Added aws cerdentials in app.py for policy gate

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+AWS_REGION = "ap-south-1"
 @app.route("/")
 def home():
     return "Enterprise DevSecOps Pipeline Running"
